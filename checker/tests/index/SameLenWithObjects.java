679154321f09b2298775145d84e62108ba9cb9d8
Martin Kellogg <kelloggm@cs.washington.edu>
2017-03-29 13:46:33 -0700

    test that actually passes

--- checker/tests/index/SameLenWithObjects.java
+++ checker/tests/index/SameLenWithObjects.java
@@ -6,13 +6,43 @@ class SameLenWithObjects {
         Object[] var_infos;
     }
 
-    static final class Invocation1 {
+    static final class Invocation0 {
         SimpleCollection sc;
-        Object @SameLen({"vals1", "this.sc.var_infos"}) [] vals1;
+        Object @SameLen({"vals1", "sc.var_infos"}) [] vals1;
 
         void format1() {
             for (int j = 0; j < vals1.length; j++) {
                 System.out.println(sc.var_infos[j]);
+            }
+        }
+    }
+
+    static final class PptTopLevel {
+        final String[] var_infos;
+
+        PptTopLevel(String[] var_infos) {
+            this.var_infos = var_infos;
+        }
+    }
+
+    static final class Invocation1 {
+        PptTopLevel ppt;
+        Object @SameLen({"vals1", "ppt.var_infos"}) [] vals1;
+
+        void format1() {
+            for (int j = 0; j < vals1.length; j++) {
+                System.out.println(ppt.var_infos[j]);
+            }
+        }
+    }
+
+    static final class Invocation2 {
+        Object @SameLen({"vals2", "mods2"}) [] vals2;
+        int @SameLen({"vals2", "mods2"}) [] mods2;
+
+        void format2() {
+            for (int j = 0; j < vals2.length; j++) {
+                System.out.println(mods2[j]);
             }
         }
     }
